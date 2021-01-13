f5a89f97c26b4f807b1267a40cc3e5a840a4fba6
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 15:09:45 +0900

    Remove Subscriber Badge option

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,5 @@
 let checkboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type=checkbox][name=badge]');
+let delegation = document.getElementById('delegation');
 
 chrome.storage.local.get(['badge_setting'], function (result) {
     console.log(JSON.stringify(result));
@@ -16,52 +17,40 @@ chrome.storage.local.get(['badge_setting'], function (result) {
 
 });
 
-checkboxes.forEach(ckbox => {
-    ckbox.addEventListener('change', e => {
-        let badge_list: Array<string> = [];
-        let badge_setting: Array<string> = [];
+if (delegation) {
+    delegation.addEventListener('change', e => {
+        let target = <HTMLInputElement>e.target;
+        if (target.getAttribute('name') === 'badge' || target.getAttribute('checkbox')) {
+            let badge_list: Array<string> = [];
+            let badge_setting: Array<string> = [];
 
-        checkboxes.forEach(c => {
-            if (c.checked) {
-                const id = c.id;
-                if (badge_lang.ko[id]) {
-                    badge_setting.push(id);
-                    badge_list.push(badge_lang.ko[id]);
+            checkboxes.forEach(c => {
+                if (c.checked) {
+                    const id = c.id;
+                    if (badge[id]) {
+                        badge_setting.push(id);
+                        badge_list.push(badge[id][0], badge[id][1]);
+                    }
                 }
-            }
-        });
+            });
 
-        chrome.storage.local.set({ badge_list: badge_list, badge_setting: badge_setting }, function () {
-            console.log('Value is set to ' + JSON.stringify({ badge_list: badge_list, badge_setting: badge_setting }));
-        });
-    });
-});
+            chrome.storage.local.set({ badge_list: badge_list, badge_setting: badge_setting }, function () {
+                console.log('Value is set to ' + JSON.stringify({ badge_list: badge_list, badge_setting: badge_setting }));
+            });
+        }
 
-type Lang = {
-    [key: string]: string;
-    streamer: string;
-    manager: string;
-    vip: string;
-    verified: string;
-    subscriber: string;
+    })
 }
 type Translator = {
-    ko: Lang;
-    en: Lang;
+    [key: string]: string[];
+    streamer: string[];
+    manager: string[];
+    vip: string[];
+    verified: string[];
 }
-const badge_lang: Translator = {
-    ko: {
-        streamer: '스트리머',
-        manager: '매니저',
-        vip: 'VIP',
-        verified: '인증 완료',
-        subscriber: '정기구독자'
-    },
-    en: {
-        streamer: 'Broadcaster',
-        manager: 'Moderator',
-        vip: 'VIP',
-        verified: 'Verified',
-        subscriber: 'Subscriber'
-    }
+const badge: Translator = {
+    streamer: ['스트리머', 'Broadcaster'],
+    manager: ['매니저', 'Moderator'],
+    vip: ['VIP', 'VIP'],
+    verified: ['인증 완료', 'Verified']
 }
