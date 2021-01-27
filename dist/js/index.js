efd736c4971a1f57c2785ff608b17a52c1f6c9bd
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 10:08:21 +0900

    Firefox compatibility

--- dist/js/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var element = document.createElement('script');
-element.setAttribute("defer", "defer");
-element.src = chrome.runtime.getURL('dist/js/inject_background.js');
-document.body.appendChild(element);
