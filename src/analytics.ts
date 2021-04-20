c3d16602300a4331db716b92610255552143e1a5
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 00:18:52 +0900

    add google analytics

--- src/analytics.ts
+++ src/analytics.ts
@@ -2,7 +2,7 @@ var gaNewElem : any = {};
 var gaElems : any = {};
 
 function gaInit(){
-  
+
   var currdate : any = new Date();
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -10,7 +10,9 @@ function gaInit(){
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga', gaNewElem, gaElems);
   
-  ga('create', 'UA-194964708-1', 'auto');
+  //ga('create', 'UA-194964708-1', 'auto'); for dev
+  ga('create', 'UA-194964708-2', 'auto');
+  
   ga('set', 'checkProtocolTask', null);
   ga('send', 'pageview', '/popup');
 
