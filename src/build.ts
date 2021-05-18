e4b0c7c6f4c017e281c786b7d608d7e5dfcae3b9
BuildTools <unconfigured@null.spigotmc.org>
2021-05-18 16:34:08 +0900

    Change Version 1.2.12 to 1.2.13, Add Font Awesome Library

--- src/build.ts
+++ src/build.ts
@@ -7,15 +7,18 @@ const FILES: string[] = [
   'manifest.json',
   '_locales/en/messages.json',
   '_locales/ko/messages.json',
-  'public/background.html',
   'public/popup.html',
   'dist/js/extension_background.js',
   'dist/js/inject_background.js',
   'dist/js/popup.js',
   'dist/css/inject_background.css',
   'dist/css/popup.css',
-  'public/icons/cc_icon64.png',
-  'public/icons/cc_icon128.png'
+  'dist/css/fa/brands.css',
+  'dist/css/fa/fontawesome.min.css',
+  'dist/css/fa/solid.css',
+  'public/icons/cc_icon128.png',
+  'dist/webfonts/fa-brands-400.woff2',
+  'dist/webfonts/fa-solid-900.woff2'
 ];
 
 function run() {
