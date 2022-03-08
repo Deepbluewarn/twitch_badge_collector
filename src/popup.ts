91b0bf9b0f2148b7b46fb2b805f4a8d868bf69ca
bluewarn <n5lp97@gmail.com>
2022-03-08 20:45:51 +0900

    remove badge options, add options for mini page

--- src/popup.ts
+++ src/popup.ts
@@ -1,104 +1,49 @@
-import { Filter, filter_type } from './types.js.js';
-
 (function () {
-    let options = <HTMLDivElement>document.getElementById('options');
     let add_filter_btn = <HTMLButtonElement>document.getElementById('add_filter_btn');
-    
-    let current_url = '';
-
-    let global_filter: Map<string, Filter> = new Map();
 
     function localizeHtmlPage() {
-
-        let bf_op = document.getElementsByClassName('badge_filter_option');
         let review_link = <HTMLDivElement>document.getElementById('review_link');
         let support_link = <HTMLDivElement>document.getElementById('support_link');
         let homepage_link = <HTMLDivElement>document.getElementById('homepage_link');
-        let topDisplay = <HTMLDivElement>document.getElementById('topDisplay_text');
-
-        Array.from(bf_op).forEach(e => {
-            let text = e.getElementsByClassName('text');
-            let id = text[0].getAttribute('id');
-            if (id) text[0].textContent = chrome.i18n.getMessage(id);
-        });
 
         add_filter_btn.textContent = chrome.i18n.getMessage('p_filter_btn');
-
         review_link.textContent = chrome.i18n.getMessage('review');
         support_link.textContent = chrome.i18n.getMessage('support');
         homepage_link.textContent = chrome.i18n.getMessage('homepage');
-        topDisplay.textContent = chrome.i18n.getMessage('p_topDisplay');
     }
 
     window.addEventListener('load', e => {
         localizeHtmlPage();
     });
 
-    //init popop setting value
-    chrome.storage.sync.get('filter', function (result) {
-
-        let chboxes = Array.from(document.getElementsByClassName('badge_checkbox'));
-
-        global_filter = new Map(result.filter);
-
-        chboxes.forEach(e => {
-            let id = e.getAttribute('id')!;
-            let f = global_filter.get(id)!;
-
-            (e as HTMLInputElement).checked = f.filter_type === filter_type.Include ? true : false;
-        });
-
+    chrome.storage.local.get(['position', 'theme', 'font_size', 'language'], (res) => {
+        (document.getElementById('select_language') as HTMLSelectElement).value = `language__${res.language}`;
+        (document.getElementById('select_theme') as HTMLSelectElement).value = `theme__${res.theme}`;
+        (document.getElementById('select_font-size') as HTMLSelectElement).value = res.font_size;
+        (document.getElementById('select_chat-position') as HTMLSelectElement).value = res.position;
     });
 
-    chrome.storage.local.get('topDisplay', function (result) {
-        let chboxes = <HTMLInputElement>document.getElementById('topDisplay');
-        chboxes.checked = result.topDisplay;
-    });
+    document.getElementById('setting_container')?.addEventListener('change', e=> {
+        const target = <HTMLSelectElement>e.target;
 
-    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        if(target.tagName !== 'SELECT') return;
 
-        if (namespace != 'local') return;
+        let changed = target.value;
 
-        for (var key in changes) {
-            let newValue = changes[key].newValue;
-
-            if (key === 'current_url') {
-                current_url = newValue;
-            }
-        }
-    });
-
-    //Listeners..
-
-    options.addEventListener('change', e => {
-
-        let target = <HTMLInputElement>e.target;
-
-        if(target.getAttribute('type') === 'checkbox'){
-            if(target.getAttribute('name') === 'badge'){
-                const id = target.getAttribute('id')!;
-                const checked = target.checked;
-                const type = checked ? filter_type.Include : filter_type.Exclude;
-    
-                let filter: Filter | undefined = global_filter.get(id);
-                if(!filter){
-                    target.checked = !checked;
-                    return;
-                }
-                filter.filter_type = type;
-                global_filter.set(id, filter);
-    
-                chrome.storage.sync.set({ filter: Array.from(global_filter) }, () => { });
-            }else if(target.getAttribute('name') == 'topDisplay'){
-                const type = target.checked;
-
-                chrome.storage.local.set({topDisplay : type});
-            }
+        if(target.id === 'select_language'){
+            changed = changed.substring(changed.lastIndexOf('_') + 1);
+            chrome.storage.local.set({language : changed});
+        }else if(target.id === 'select_theme'){
+            changed = changed.substring(changed.lastIndexOf('_') + 1);
+            chrome.storage.local.set({theme : changed});
+        }else if(target.id === 'select_font-size'){
+            chrome.storage.local.set({font_size : changed});
+        }else if(target.id === 'select_chat-position'){
+            chrome.storage.local.set({position : changed});
         }
     });
 
     add_filter_btn.addEventListener('click', e => {
-        let url = chrome.runtime.getURL('public/filter.html');
-        chrome.tabs.create({ url: url });
+        chrome.tabs.create({ url: 'https://wtbc.bluewarn.dev/setting/filter' });
     });
 })();
