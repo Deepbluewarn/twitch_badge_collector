7cd919a98e43a0c7916d07fc5d60a3e58ef30903
bluewarn <n5lp97@gmail.com>
2022-03-10 15:01:00 +0900

    move badge filter options to the settings page.
    Filters for new users are now initialized in the filter settings page.

--- src/extension_background.ts
+++ src/extension_background.ts
@@ -1,17 +1,7 @@
-function get_new_filter() {
-    const filter = new Map();
-    filter.set('streamer', { filter_id: 'streamer', category: 'badge_uuid', filter_type: 'include', value: '5527c58c-fb7d-422d-b71b-f309dcb85cc1', note: '스트리머' });
-    filter.set('manager', { filter_id: 'manager', category: 'badge_uuid', filter_type: 'include', value: '3267646d-33f0-4b17-b3df-f923a41db1d0', note: '매니저' });
-    filter.set('vip', { filter_id: 'vip', category: 'badge_uuid', filter_type: 'include', value: 'b817aba4-fad8-49e2-b88a-7cc744dfa6ec', note: 'VIP' });
-    filter.set('verified', { filter_id: 'verified', category: 'badge_uuid', filter_type: 'include', value: 'd12a2e27-16f6-41d0-ab77-b780518f00a3', note: '인증 완료' });
-
-    return filter;
-}
-
 chrome.runtime.onInstalled.addListener(function (reason: any) {
     chrome.storage.local.get(['position', 'theme', 'font_size', 'language'], (res) => {
         const language = res.language ? res.language : chrome.i18n.getMessage('language');
-        const theme = res.theme ? res.theme : 'dark';
+        const theme = res.theme ? res.theme : 'light';
         const font_size = res.font_size ? res.font_size : 'default';
         const position = res.position ? res.position : 'position-down';
 
