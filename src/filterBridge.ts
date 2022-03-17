04ba50d1c285cac0590cc617228f9c6e858a3634
bluewarn <n5lp97@gmail.com>
2022-03-18 01:50:51 +0900

    Fix: Send msg_id even if there is no filter object

--- src/filterBridge.ts
+++ src/filterBridge.ts
@@ -1,7 +1,16 @@
 import browser from "webextension-polyfill";
 
 const msg_id = Math.random().toString(36).substring(2,12);
+const messageIdChannel = new BroadcastChannel('MessageId');
 const filterChannel = new BroadcastChannel('Filter');
+
+// storage 에 filter 가 없어도 msg_id 는 전달해야 함.
+const msg = {
+    from : 'tbc',
+    to : ['wtbc-main', 'wtbc-filter', 'wtbc-mini'],
+    msg_id : msg_id
+}
+messageIdChannel.postMessage(msg);
 
 browser.storage.sync.get('filter').then(result => {
     const msg = {
